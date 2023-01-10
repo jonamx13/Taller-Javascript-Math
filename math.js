ba1e4c61137a1480e7701ac31df9c34752808412
[jonamx13] <[lav.jonathanom@gmail.com]>
2023-01-10 09:58:24 -0700

    Formulas para circulos

--- math.js
+++ math.js
@@ -48,3 +48,31 @@ console.log({
 });
 
 console.groupEnd('Triangulo');
+
+console.group('Circulo');
+
+const radioCirculo = 3;
+const diamertroCirculo = radioCirculo * 2;
+const PI = 3.1415;
+
+const circunferencia = diamertroCirculo * PI;
+const areaCirculo = (radioCirculo ** 2) * PI;
+
+console.log ({
+  radioCirculo,
+  diamertroCirculo,
+  PI,
+  circunferencia,
+  areaCirculo,
+});
+
+function calcularCirculo(radio) {
+  const diametro = radio * 2;
+  const radioAlCuadrado = Math.pow(radio, 2);
+  return {
+    circunferencia: diametro * Math.PI.toFixed(2),
+    area: radioAlCuadrado * Math.PI.toFixed(2),
+  }
+}
+
+console.groupEnd('Circulo');
